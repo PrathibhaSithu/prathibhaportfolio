a4c278d64fbf375d28ddd569d5d2ee4c9ed62aa3
PrathiSithu <86074406+PrathibhaSithu@users.noreply.github.com>
2025-07-17 11:43:00 +0530

    Add Footer and SmallSection components to layout
    Introduced a new Footer component with newsletter subscription and site links, and added a placeholder SmallSection component. Updated App.jsx to include these new sections at the end of the page layout.

--- prathibhaportfolio/src/App.jsx
+++ prathibhaportfolio/src/App.jsx
@@ -1,10 +1,12 @@
 import AnimatedCounter from "./components/AnimatedCounter";
 import FadeInSection from "./components/FadeInSection";
+import SmallSection from "./components/SmallSection";
 import TopNavBar from "./components/TopNavBar";
 import ContactMe from "./sections/ContactMe";
 import Education from "./sections/Education";
 import Experience from "./sections/Experience";
 import FeatureCards from "./sections/FeatureCards";
+import Footer from "./sections/Footer";
 import Hero from "./sections/Hero";
 import AppShowcase from "./sections/ShowcaseSection";
 import TecSkills from "./sections/TecSkills";
@@ -20,6 +22,8 @@ const App = () => (
     <FadeInSection><FeatureCards /></FadeInSection>
     <FadeInSection><AppShowcase/></FadeInSection>
     <FadeInSection><ContactMe /></FadeInSection>
+    <FadeInSection><SmallSection/></FadeInSection>
+    <FadeInSection><Footer/></FadeInSection>
     
   </>
 );
