0e9bdca98b25f9b33ab10850f43a3725bc5011bf
PrathiSithu <86074406+PrathibhaSithu@users.noreply.github.com>
2025-07-17 09:54:01 +0530

    Refactor section imports and enable components
    Renamed 'section' directory to 'sections' for consistency and updated all related imports in App.jsx. Also enabled previously commented-out components and added new dependencies 'countup' and 'react-responsive' to package.json.

--- prathibhaportfolio/src/App.jsx
+++ prathibhaportfolio/src/App.jsx
@@ -1,28 +1,25 @@
-// import AnimatedCounter from "./components/AnimatedCounter";
-// import FadeInSection from "./components/FadeInSection";
-// import TopNavBar from "./components/TopNavBar";
-// import ContactMe from "./sections/ContactMe";
-// import Education from "./sections/Education";
-// import Experience from "./sections/Experience";
-// import FeatureCards from "./sections/FeatureCards";
-// import Hero from "./sections/Hero";
-// import AppShowcase from "./sections/ShowcaseSection";
-// import TecSkills from "./sections/TecSkills";
-
+import AnimatedCounter from "./components/AnimatedCounter";
 import FadeInSection from "./components/FadeInSection";
-import Hero from "./section/Hero";
+import TopNavBar from "./components/TopNavBar";
+import ContactMe from "./sections/ContactMe";
+import Education from "./sections/Education";
+import Experience from "./sections/Experience";
+import FeatureCards from "./sections/FeatureCards";
+import Hero from "./sections/Hero";
+import AppShowcase from "./sections/ShowcaseSection";
+import TecSkills from "./sections/TecSkills";
 
 const App = () => (
   <>
-    {/* <TopNavBar /> */}
+    <TopNavBar />
     <FadeInSection><Hero /></FadeInSection>
-    {/* <FadeInSection><AnimatedCounter /></FadeInSection>
+    <FadeInSection><AnimatedCounter /></FadeInSection>
     <FadeInSection><Experience /></FadeInSection>
     <FadeInSection><Education /></FadeInSection>
     <FadeInSection><TecSkills /></FadeInSection>
     <FadeInSection><FeatureCards /></FadeInSection>
     <FadeInSection><AppShowcase/></FadeInSection>
-    <FadeInSection><ContactMe /></FadeInSection> */}
+    <FadeInSection><ContactMe /></FadeInSection>
     
   </>
 );
