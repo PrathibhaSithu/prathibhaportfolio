845322c1b779d86d08d170a76f9de827f1abca10
PrathiSithu <86074406+PrathibhaSithu@users.noreply.github.com>
2025-07-21 15:55:36 +0530

    Update Experience.jsx

--- prathibhaportfolio/src/sections/Experience.jsx
+++ prathibhaportfolio/src/sections/Experience.jsx
@@ -56,7 +56,7 @@ const Experience = () => {
   return (
     <section
       id="experience"
-      className="bggradient flex-center  md:mb-6  section-padding xl:px-0"
+      className="bggradient flex-center md:mb-6 section-padding xl:px-0"
     >
       <div className="w-full h-full md:px-20 px-5 mt-10">
         <TitleHeader
